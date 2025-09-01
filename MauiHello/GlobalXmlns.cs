09792f9215707cd0c7175888c7b92dc98bac39a5
GuttiDK <93974633+GuttiDK@users.noreply.github.com>
2025-09-01 12:12:26 +0200

    Add project files.

---
+++ MauiHello/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiHello")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiHello.Pages")]
